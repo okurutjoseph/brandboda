bf0707c45f4609d047e00d6f9dd750a4d2ff8f23
okurutjoseph <okurutjoseph97@gmail.com>
2025-02-09 18:26:39 +0300

    commit

--- app/api/uploadthing/route.ts
+++
@@ -1,6 +0,0 @@
-import { createNextRouteHandler } from "uploadthing/next";
-import { uploadRouter } from "./core";
- 
-export const { GET, POST } = createNextRouteHandler({
-  router: uploadRouter,
-});
