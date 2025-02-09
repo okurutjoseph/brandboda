683b4559a84f1281b38d549c6276a10b52ceef4d
okurutjoseph <okurutjoseph97@gmail.com>
2025-02-09 18:17:11 +0300

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import { Inter } from 'next/font/google';
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import { cn } from "@/lib/utils";
-import { ClerkProvider } from '@clerk/nextjs';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -25,21 +24,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
-      <html lang="en" suppressHydrationWarning>
-        <head />
-        <body className={cn("min-h-screen bg-black font-sans antialiased", inter.className)}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-            <Toaster />
-          </ThemeProvider>
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn("min-h-screen bg-black font-sans antialiased", inter.className)}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <Toaster />
+        </ThemeProvider>
+      </body>
+    </html>
   );
 }
--- convex/auth.config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-    providers: [
-      {
-        domain: "https://brief-raccoon-32.clerk.accounts.dev/",
-        applicationID: "convex",
-      },
-    ]
-  };
